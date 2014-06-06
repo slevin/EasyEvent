a2b9bebe2ffaba5cbf3fb75fc56505097a167799
Sean Levin <sean.levin@gmail.com>
2014-06-05 20:05:25 -0400

    Initial Commit

---
+++ EasyEventTests/EasyEventTests.m
@@ -0,0 +1,34 @@
+//
+//  EasyEventTests.m
+//  EasyEventTests
+//
+//  Created by Sean Levin on 6/5/14.
+//  Copyright (c) 2014 Sean Levin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EasyEventTests : XCTestCase
+
+@end
+
+@implementation EasyEventTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
